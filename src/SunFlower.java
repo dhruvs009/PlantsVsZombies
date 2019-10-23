0597d9e2b81c2183159ea25468a94b7e8740580f
sargamm <sargam18262@iiitd.ac.in>
2019-10-23 23:16:41 +0530

    SunProvider SunFlower init, incomplete Shooter init

---
+++ src/SunFlower.java
@@ -0,0 +1,8 @@
+public class SunFlower extends SunProvider{
+    public SunFlower(Image Appearance){
+        super(100,50,7.5,Appearance);
+    }
+    public Plants plantThis(){
+        
+    }
+}
