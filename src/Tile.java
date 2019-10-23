56acfda3bdcd3fd00d3384ee79f485bb8dcdb600
dhruvsahnan1008 <dhruv.ds.009@gmail.com>
2019-10-23 23:02:37 +0530

    Land init

--- src/Tile.java
+++ src/Tile.java
@@ -3,5 +3,12 @@ public abstract class Tile{
     protected Image tileAppearance;
     public Tile(Image tileAppearance){
         this.tileAppearance=tileAppearance;
+        this.toPlantHere=null;
+    }
+    protected void setPlant(Plants toPlantHere){
+        this.toPlantHere=toPlantHere;
+    }
+    protected Plants getPlant(){
+        return this.toPlantHere;
     }
 }
